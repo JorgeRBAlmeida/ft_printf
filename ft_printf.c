e4eb32d05a61e23a32d0984a09c47d6697d583d8
C1322989 Jorge Rodrigo Barbosa Almeida <c1322989@interno.bb.com.br>
2022-06-23 11:42:06 -0300

    refactoring ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 15:10:15 by joalmeid          #+#    #+#             */
-/*   Updated: 2022/06/23 08:40:37 by joalmeid         ###   ########.fr       */
+/*   Updated: 2022/06/23 09:07:24 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,68 @@
 #include <stdio.h>
 
 size_t		ft_strlen(const char *src);
-static int	print(const char *src, va_list params);
-static int	print_param(char c, va_list params);
+static int	print_arg(char c, va_list args);
 
 size_t		ft_strlen(const char *src);
 char		*ft_strchr(const char *s, int c);
 int			ft_putstr_fd(char *s, int fd);
 int			ft_putchar_fd(char c, int fd);
-static int	putnbase_len(unsigned long n, int base);
+static int	putnbase_len(long long n, int base);
 int			ft_putnbase_fd(long long nbr, int fd, int base, char x);
-void		*ft_calloc(size_t num, size_t size);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		ft_bzero(void *s, size_t n);
 
 int	ft_printf(const char *src, ...)
 {
-	va_list	params;
+	va_list	args;
 	int		printed;
+	int		index;
 
+	index = -1;
+	printed = 0;
 	if (!src)
 		return (0);
-	va_start(params, src);
-	printed = print(src, params);
-	va_end(params);
+	va_start(args, src);
+	while (src[++ index])
+	{
+		if (src[index] == '%')
+		{
+			index ++;
+			if (ft_strchr("cspdiuxX%", src[index]))
+				printed += print_arg(src[index], args);
+			if (!src[index])
+				break ;
+		}
+		else
+			ft_putchar_fd(src[index], 1);
+	}
+	va_end(args);
+	printed += index;
 	return (printed);
+}
+
+/* STATICS */
+
+static int	print_arg(char c, va_list args)
+{
+	if(c == '%')
+		return (ft_putchar_fd('%', 1) - 1);
+	else if(c == 'c')
+		return (ft_putchar_fd((va_arg(args, int)), 1) - 2);
+	else if(c == 's')
+		return (ft_putstr_fd((va_arg(args, char *)), 1) - 2);
+	else if(c == 'd' || c == 'i')
+		return (ft_putnbase_fd(va_arg(args, int), 1, 10, 'x') - 2);
+	else if(c == 'u')
+		return (ft_putnbase_fd(va_arg(args, unsigned int), 1, 10, 'x') - 2);
+	else if(c == 'x')
+		return (ft_putnbase_fd(va_arg(args, unsigned int), 1, 16, 'x') - 2);
+	else if(c == 'X')
+		return (ft_putnbase_fd(va_arg(args, unsigned int), 1, 16, 'X') - 2);
+	else if(c == 'p')
+	{
+		write(1, "0x", 2);
+		return (ft_putnbase_fd(va_arg(args, unsigned long), 1, 16, 'x'));
+	}
+	return (0);
 }
 
 /* LIBFT */
@@ -130,116 +168,18 @@ int	ft_putstr_fd(char *s, int fd)
 	return (len);
 }
 
-void	*ft_calloc(size_t num, size_t size)
+/* int	main(void)
 {
-	void	*buffer;
-	int		total;
+	//int	t = -10000;
+	//char str[] = "hello!";
+	// ft_printf("\nreturn ft_printf:\t|%i|\n", ft_printf("'%%%'"));
+	// printf("\nreturn printf:\t|%i|\n", printf("'%%%'"));
+	//void *p;
+	//ft_printf("\nreturn ft_printf:\t|%i|\n", ft_printf("%p", p));
+	//printf("\nreturn printf:\t|%i|\n", printf("%p", p));
 
-	if (num == 0 || size == 0)
-	{
-		num = 1;
-		size = 1;
-	}
-	total = num * size;
-	buffer = malloc(total);
-	if (buffer == NULL)
-		return (NULL);
-	ft_bzero(buffer, total);
-	return (buffer);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	const char	*s;
-	char		*d;
-
-	s = src;
-	d = dst;
-	if (!s && !d && n > 0)
-		return (NULL);
-	while (n)
-	{
-		*d ++ = *s ++;
-		n --;
-	}
-	return (dst);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*dst;
-
-	dst = s;
-	while (n > 0)
-	{
-		*dst = '\0';
-		n --;
-		dst ++;
-	}
-}
-
-/* STATICS */
-
-static int	print(const char *src, va_list params)
-{
-	int		index;
-	int		printed;
-
-	index = -1;
-	printed = 0;
-	while (src[++ index])
-	{
-		if (src[index] == '%')
-		{
-			index ++;
-			if (ft_strchr("cspdiuxX%", src[index]))
-			{
-				printed += print_param(src[index], params);
-			}
-			if (!src[index])
-				break ;
-		}
-		else
-			write(1, &src[index], 1);
-	}
-	printed += index;
-	return (printed);
-}
-
-/* • %c Prints a single character.
-CHECK• %s Prints a string (as defined by the common C convention).
-CHECK• %% Prints a percent sign. 
-CHECK• %d Prints a decimal (base 10) number.
-CHECK• %i Prints an integer in base 10.
-CHECK• %u Prints an unsigned decimal (base 10) number.
-• %p The void * pointer argument has to be printed in hexadecimal format.
-• %x Prints a number in hexadecimal (base 16) lowercase format.
-• %X Prints a number in hexadecimal (base 16) uppercase format.
-*/
-
-static int	print_param(char c, va_list params)
-{
-	if(c == '%')
-		return (write(1, "%", 1) - 1);
-	else if(c == 'c')
-		return (ft_putchar_fd((va_arg(params, int)), 1) - 2);
-	else if(c == 's')
-		return (ft_putstr_fd((va_arg(params, char *)), 1) - 2);
-	else if(c == 'd' || c == 'i')
-		return (ft_putnbase_fd(va_arg(params, int), 1, 10, 'x') - 2);
-	else if(c == 'u')
-		return (ft_putnbase_fd(va_arg(params, unsigned int), 1, 10, 'x') - 2);
-	else if(c == 'x')
-		return (ft_putnbase_fd(va_arg(params, unsigned int), 1, 16, 'x') - 2);
-	else if(c == 'X')
-		return (ft_putnbase_fd(va_arg(params, unsigned int), 1, 16, 'X') - 2);
-	else if(c == 'p')
-	{
-		write(1, "0x", 2);
-		return (ft_putnbase_fd(va_arg(params, unsigned long), 1, 16, 'x'));
-	}
-	return (0);
-}
+	//printf("%d", ft_printf("test%s", str));
+} */
 
 /* int	ft_isascii(int c)
 {
@@ -272,16 +212,3 @@ int	main(void)
 		src[1] ++;
 	}
 } */
-
-int	main(void)
-{
-	//int	t = -10000;
-	//char str[] = "hello!";
-	// ft_printf("\nreturn ft_printf:\t|%i|\n", ft_printf("'%%%'"));
-	// printf("\nreturn printf:\t|%i|\n", printf("'%%%'"));
-	void *p;
-	ft_printf("\nreturn ft_printf:\t|%i|\n", ft_printf("%p", p));
-	printf("\nreturn printf:\t|%i|\n", printf("%p", p));
-
-	//printf("%d", ft_printf("test%s", str));
-}
