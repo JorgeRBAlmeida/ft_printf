53accbcf4d01c1e91eb9430cde0f364c0d59ae81
C1322989 Jorge Rodrigo Barbosa Almeida <c1322989@interno.bb.com.br>
2022-06-24 09:57:12 -0300

    putting mod libft funcs in new file

---
+++ ft_printf_utils.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 09:10:35 by joalmeid          #+#    #+#             */
+/*   Updated: 2022/06/24 09:32:30 by joalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlen(const char *src)
+{
+	size_t	index;
+
+	index = 0;
+	while (src && src[index])
+	{
+		index ++;
+	}
+	return (index);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (*s == '\0' && (char)c != *s)
+			return (0);
+		s++;
+	}
+	return ((char *) s);
+}
+
+int	ft_putchar_len(char c, int fd)
+{
+		write(fd, &c, 1);
+		return (1);
+}
+
+int	ft_putnbr_len(long n, int fd)
+{
+	int	len;
+	
+	len = putnbr_len(n);
+	if (n < 0)
+	{
+		n = -n;
+		ft_putchar_len('-', fd);
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar_len((n + '0'), fd);
+	else
+	{
+		ft_putnbr_len((n / 10), fd);
+		ft_putnbr_len((n % 10), fd);
+	}
+	return (len);
+}
+
+int	ft_putnbase_u(unsigned long nbr, int fd, int base, char type)
+{
+	int	len;
+
+	if (nbr >= 0 && nbr <= 9)
+		ft_putchar_len((nbr + 48), fd);
+	else if ((base == 16) && (type == 'x') && (nbr >= 10 && nbr <= 15))
+		ft_putchar_len((nbr + 87), fd);
+	else if ((base == 16) && (type == 'X') && (nbr >= 10 && nbr <= 15))
+		ft_putchar_len((nbr + 55), fd);
+	else
+	{
+		ft_putnbase_u((nbr / base), fd, base, type);
+		ft_putnbase_u((nbr % base), fd, base, type);
+	}
+	len = putnbase_u_len(nbr, base);
+	return (len);
+}
+
+int	putnbase_u_len(unsigned long n, unsigned long base)
+{
+	int	len;
+
+	len = 1;
+	while (n > (base - 1))
+	{
+		n = n / base;
+		len ++;
+	}
+	return (len);
+}
+
+int	putnbr_len(long n)
+{
+	int	len;
+
+	len = 1;
+	if (n < 0)
+	{
+		len ++;
+		n = -n;
+	}
+	while (n > 9)
+	{
+		n = n / 10;
+		len ++;
+	}
+	return (len);
+}
+
+int	ft_putstr_len(char *s, int fd)
+{
+	size_t	len;
+
+	len = 0;
+	if (s == NULL)
+	{
+		write(fd, "(null)", 6);
+		return (6);
+	}
+	len = ft_strlen(s);
+	write(fd, s, len);
+	return (len);
+}
